c69f7a1056c93f7d74be646aad9328547270f27e
wbkch <waleedbinkhalidch@gmail.com>
2022-06-22 09:38:20 +0500

    added tabbed functionality at component 3

--- bankList 2/script.js
+++ bankList 2/script.js
@@ -49,6 +49,29 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
   }
 });
 
+//tabbed components
+
+const tabs= document.querySelectorAll('.operations__tab');
+const tabsContainer= document.querySelector('.operations__tab-container');
+const tabsContent= document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function(e){
+  const clicked = e.target.closest('.operations__tab')
+  if (!clicked) return;
+ 
+
+  //Remove active classes
+  tabs.forEach(t=>t.classList.remove('operations__tab--active'))
+  tabsContent.forEach(tc=>tc.classList.remove('operations__content--active'))
+
+  //add active classes
+  clicked.classList.add('operations__tab--active');
+  document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active')
+
+  console.log(clicked)
+})
+
+
 // #########################################
 
 // btnScrollTo.addEventListener('click', function (e) {
