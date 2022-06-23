43da1b0cd33d98a856f2642454a171017d5f8817
wbkch <waleedbinkhalidch@gmail.com>
2022-06-23 08:00:13 +0500

    lazy loading images added

--- bankList 2/script.js
+++ bankList 2/script.js
@@ -113,9 +113,8 @@ const allSections = document.querySelectorAll('.section');
 const revealSection = function (entries, observer) {
   const [entry] = entries;
   if (!entry.isIntersecting) return;
-
   entry.target.classList.remove('section--hidden');
-  observer.unobserve(entry.target);
+  observer.unobserve(entry);
 };
 
 const sectionObserver = new IntersectionObserver(revealSection, {
@@ -126,6 +125,27 @@ allSections.forEach(function (section) {
   sectionObserver.observe(section);
   section.classList.add('section--hidden');
 });
+
+//lazy loading images
+const imgTargets = document.querySelectorAll('img[data-src]');
+
+const loading = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.src = entry.target.dataset.src;
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
+  observer.unobserve(entry.target)
+};
+
+const imgObserver = new IntersectionObserver(loading, {
+  root: null,
+  threshold: .3,
+  rootMargin: '200px',
+});
+
+imgTargets.forEach(img=>imgObserver.observe(img))
 
 // const obsCallback= (enteries,observer)=>{
 //   enteries.forEach((entry)=>{
