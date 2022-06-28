2e30cc8ba254dd303eca3b7f759c1aca9f487762
wbkch <waleedbinkhalidch@gmail.com>
2022-06-28 09:46:21 +0500

    by using leaflet library including map

--- mapty/script.js
+++ mapty/script.js
@@ -17,7 +17,20 @@ if (navigator.geolocation)
       //   console.log(position);
       const { latitude } = position.coords;
       const { longitude } = position.coords;
-      console.log(`https://www.google.com/maps/@${latitude},${longitude}`)
+      // console.log(`https://www.google.com/maps/@${latitude},${longitude}`)
+
+      const coords=[latitude,longitude]
+      const map = L.map('map').setView(coords, 15);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+          attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      }).addTo(map);
+      
+      L.marker([51.5, -0.09]).addTo(map)
+          .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+          .openPopup();
+
+
     },
     function () {
       alert('block');
