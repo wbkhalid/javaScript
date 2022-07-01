20a227db1355c653203b6b23e633326c3a5bdc43
wbkch <waleedbinkhalidch@gmail.com>
2022-07-01 11:24:27 +0500

    adding popup on click and remains open

--- mapty/script.js
+++ mapty/script.js
@@ -14,23 +14,38 @@ const inputElevation = document.querySelector('.form__input--elevation');
 if (navigator.geolocation)
   navigator.geolocation.getCurrentPosition(
     function (position) {
-      //   console.log(position);
       const { latitude } = position.coords;
       const { longitude } = position.coords;
-      // console.log(`https://www.google.com/maps/@${latitude},${longitude}`)
+      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
 
-      const coords=[latitude,longitude]
+      const coords = [latitude, longitude];
       const map = L.map('map').setView(coords, 15);
 
       L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
-          attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
+
+
       
-      L.marker([51.5, -0.09]).addTo(map)
-          .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+      map.on('click', function (mapEvent) {
+        // console.log(a)
+        const {lat,lng}= mapEvent.latlng
+        L.marker([lat,lng])
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+
+              maxWidth: 200,
+              minWidth:50,
+              autoClose:false,
+              closeOnClick:false,
+              className:'running-popup',
+            })
+          )
+          .setPopupContent('worked')
           .openPopup();
-
-
+      });
     },
     function () {
       alert('block');
